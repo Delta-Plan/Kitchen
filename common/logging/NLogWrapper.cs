2658d5744de46183514aaa044be3af049c3588c7
S.Rozhin <S.Rozhin@skbkontur.ru>
2014-05-13 18:37:40 +0600

    more logging

--- common/logging/NLogWrapper.cs
+++ common/logging/NLogWrapper.cs
@@ -1,24 +1,62 @@
-﻿using NLog;
+﻿using System;
+using NLog;
 
 namespace common.logging
 {
     public class NLogWrapper : ILogger//todo s.rozhin
     {
         private Logger _nLogLogger;
-        private NLogWrapper(Logger nLogLogger)
+        private static ILogger _logWrapper;
+        private NLogWrapper()
         {
+            var nLogLogger = LogManager.GetCurrentClassLogger();
             _nLogLogger = nLogLogger;
+            _nLogLogger.LoggerReconfigured += _nLogLogger_LoggerReconfigured;
+        }
+
+        void _nLogLogger_LoggerReconfigured(object sender, EventArgs e)
+        {
+            _nLogLogger = LogManager.GetCurrentClassLogger();
         }
 
         public static ILogger GetNLogWrapper()
-         {
-             var nLogLogger = LogManager.GetCurrentClassLogger();
-             return new NLogWrapper(nLogLogger);
-         }
+        {
+            return _logWrapper ?? (_logWrapper = new NLogWrapper());
+        }
 
         public void Info(string message)
         {
             _nLogLogger.Info(message);
+        }
+
+        public void Debug(string message)
+        {
+            _nLogLogger.Debug(message);
+        }
+
+        public void Warn(string message)
+        {
+            _nLogLogger.Warn(message);
+        }
+
+        public void Error(string message)
+        {
+            _nLogLogger.Error(message);
+        }
+
+        public void Fatal(string message)
+        {
+            _nLogLogger.Fatal(message);
+        }
+
+        public void ErrorException(string message, Exception exception)
+        {
+            _nLogLogger.ErrorException(message,exception);
+        }
+
+        public void FatalException(string message, Exception exception)
+        {
+            _nLogLogger.FatalException(message, exception);
         }
     }
 }
