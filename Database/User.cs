edc95a2bd607123daf0345d32eeea393534fe45a
delta.plan <delta.plan473@gmail.com>
2014-05-15 00:38:09 +0600

    try LINQ...
    some UnitTests...

---
+++ Database/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+
+namespace Database
+{
+    [Table(Name="Users")]
+    public class User
+    {
+        [Column(IsPrimaryKey = true)] public int Id;
+
+        [Column] public string Name;
+
+        [Column] public int RoleId;
+    }
+}
