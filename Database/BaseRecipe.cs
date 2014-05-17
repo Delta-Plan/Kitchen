2c1a2d58d450824df4776c432ef59b3993750158
S.Rozhin <S.Rozhin@skbkontur.ru>
2014-05-17 13:04:52 +0600

    first iteration start

--- Database/BaseRecipe.cs
+++ Database/BaseRecipe.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+using System.Collections.Generic;
 using System.Data.Linq.Mapping;
 
 namespace Database
@@ -6,10 +8,23 @@ namespace Database
     [Table(Name = "Recipes")]
     public abstract class BaseRecipe
     {
-        [Column(IsPrimaryKey = true)] public int Id;
+        [Column(IsPrimaryKey = true)]
+        public int Id;
 
-        [Column] public string Name;
+        [Column]
+        public string Name;
 
-        [Column] public string Description;
+        [Column]
+        public string Description;
+
+        [Column(Name = "IngridientsJson")]
+        private string _ingridientsJson;
+
+
+        public Dictionary<int, string> Ingridients
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
     }
 }
---
+++ common/Singleton/GenericSingleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using common.logging;
+
+namespace common.Singleton
+{
+    public class GenericSingleton
+    {
+        public T Instance<T>(ILogger logger)
+        {
+            return default(T);
+        }
+    }
+}
