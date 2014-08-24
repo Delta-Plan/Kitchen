05fb718d0e4547d1c91b075a3cc4950d67fd124e
bazhenova-a-v <bazhenova-a-v@mail.ru>
2014-08-24 14:01:44 +0600

    add more measure type

--- Database/Ingredients/MeasureType.cs
+++ Database/Ingredients/MeasureType.cs
@@ -4,6 +4,13 @@
     {
         AtTaste = 0,
         TeaSpoon = 1,
-        Item = 2
+        SoupSpoon=2,
+        Glass = 3,
+        Thimbleful = 4,//щепотка
+        Gram = 5,
+        Kilo = 6,
+        Milliliter = 7,
+        Liter = 8,
+        Item = 9// это я так поняла шт.
     }
 }
