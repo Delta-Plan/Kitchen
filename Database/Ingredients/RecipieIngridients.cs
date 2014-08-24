5004f9ac0876d6b42bd7b3036609c8464e81c3d3
bazhenova-a-v <bazhenova-a-v@mail.ru>
2014-08-24 15:35:09 +0600

    serialize/Deserialize Json string

--- Database/Ingredients/RecipieIngridients.cs
+++ Database/Ingredients/RecipieIngridients.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using common;
 using common.Logging;
 
@@ -18,13 +19,50 @@ namespace Database.Ingredients
 
         public string SerialiseToJsonString()
         {
-            return "Todo";
+            String json = String.Empty;
+            
+            try
+            {
+                 json =(string) new JavaScriptSerializer().Serialize(this.Components);
+            }
+            catch (InvalidOperationException longReciepException)
+            {
+                ILogger Logger = NLogWrapper.GetNLogWrapper();
+                Logger.Error(String.Format("A lot of ingredients in recipe. Can't convert to JSON string from object RecipieIngridients\n{0}",
+                            longReciepException.Message));
+                
+            }
+            catch (ArgumentException longReciepException)
+            {
+                ILogger Logger = NLogWrapper.GetNLogWrapper();
+                Logger.Error(String.Format("Limit exceeded on recursion. Can't convert to JSON string from object RecipieIngridients\n{0}",
+                            longReciepException.Message));
+
+            }
+
+            
+            return json;
             //throw new NotImplementedException();
         }
         public RecipieIngridients(){}
         public RecipieIngridients(string str)
         {
-            //throw new NotImplementedException();
+
+            try
+            {
+                var Obj = (IList<Component>) new JavaScriptSerializer().Deserialize(str, typeof(IList<Component>));
+                this.Components = Obj; 
+                this.Initialized = true;
+            }
+            catch (ArgumentException invalidIngridientsException)
+            {
+                this.Initialized = false;
+                ILogger Logger = NLogWrapper.GetNLogWrapper();
+                Logger.Error(String.Format("Invalid recipes ingredient in base. Can't convert from JSON string to object RecipieIngridients\n{0}",
+                            invalidIngridientsException.Message));
+
+            }
+            
         }
         public bool Initialized { get; protected set; }
     }
