8bacf997140236e4fb5717e1fead857257635172
S.Rozhin <S.Rozhin@skbkontur.ru>
2014-10-10 00:38:48 +0600

    bootstrap start: ingredient add in recipe creation

---
+++ Kitchen/Auxillary/MeasureHelperTemp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Database.Ingredients;
+
+namespace Kitchen.Auxillary
+{
+    public class MeasureHelperTemp
+    {
+        public static MeasureType measureType { get; set; }
+
+        public static IEnumerable<SelectListItem> ProductMeasureSelectList
+        {
+            get
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    yield return new SelectListItem
+                    {
+                        Value = i.ToString(),
+                        Text = ((MeasureType)i).ToString(),
+                        Selected = (int) measureType == i
+                    };
+                }
+            }
+        }
+    }
+}
